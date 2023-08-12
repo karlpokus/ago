c343acaf7bf8125b266f1abccaa933af7340b093
Karl Pokus <karl.pokus@gmail.com>
2023-08-12 23:08:40 +0200

    add cli

---
+++ cmd/ago/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/karlpokus/ago"
+)
+
+func main() {
+	log.SetFlags(0)
+	log.SetOutput(os.Stdout)
+	if len(os.Args) < 2 {
+		log.Fatal("missing time argument")
+	}
+	t, err := time.Parse(time.RFC3339, os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(ago.Parse(t))
+}
